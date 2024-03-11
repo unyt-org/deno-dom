8be8969dbd4f7ab215b6a84f839d335e490e37c8
benStre <strehle.benedikt@gmail.com>
2024-03-11 21:38:38 +0100

    add style attribute for svg element

--- src/dom/elements/svg-element.ts
+++ src/dom/elements/svg-element.ts
@@ -3,9 +3,11 @@ import { DOMStringMap } from "../dom-string-map.ts";
 import { Element } from "../element.ts";
 import { Node } from "../node.ts";
 import { SVGTag } from "../types/tags.ts";
+import { CSSStyleDeclaration } from "../../css/CSSStyleDeclaration.ts";
 
 export class SVGElement extends Element {
-	
+	style = CSSStyleDeclaration.create()
+
 	constructor(
 		tagName: SVGTag,
 		parentNode: Node | null,
@@ -19,6 +21,28 @@ export class SVGElement extends Element {
 			attributes,
 			key,
 		);
+
+		// set initial style
+		if (this.hasAttribute("style")) {
+			this.style.cssText = this.getAttribute("style")!
+		}
+
+		// bind style to style attribute	
+		this.style.onUpdate(()=>this.#handleStyleUpdate())
+		this.#handleStyleUpdate()
+	}
+
+	#handleStyleUpdate() {
+		// todo don't allow user overwrite of style attribute
+		if (this.style.cssText && !this.hasAttribute("style")) {
+			this._setAttributeDef("style", {
+				get: () => this.style.cssText,
+				set: (value: string) => {
+					this.style.cssText = value;
+					if (!value) this.removeAttribute("style");
+				},
+			})
+		}
 	}
 
 	dataset = DOMStringMap.create(this)
